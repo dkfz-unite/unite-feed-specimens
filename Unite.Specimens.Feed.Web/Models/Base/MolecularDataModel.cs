37e0b6c424674df01292cbed6dc6150ec9f1bbb1
DGitRich <daniel.rich@gmx.net>
2023-12-06 12:11:25 +0100

    Update MolecularDataModel

--- Unite.Specimens.Feed.Web/Models/Base/MolecularDataModel.cs
+++ Unite.Specimens.Feed.Web/Models/Base/MolecularDataModel.cs
@@ -1,13 +1,20 @@
-﻿using Unite.Data.Entities.Specimens.Enums;
+﻿using System.Text.Json.Serialization;
+using Unite.Data.Entities.Specimens.Enums;
 
 namespace Unite.Specimens.Feed.Web.Models.Base;
 
 public class MolecularDataModel
 {
+    [JsonPropertyName("mgmtStatus")]
     public MgmtStatus? MgmtStatus { get; set; }
+    [JsonPropertyName("idhStatus")]
     public IdhStatus? IdhStatus { get; set; }
+    [JsonPropertyName("idhMutation")]
     public IdhMutation? IdhMutation { get; set; }
+    [JsonPropertyName("geneExpressionSubtype")]
     public GeneExpressionSubtype? GeneExpressionSubtype { get; set; }
+    [JsonPropertyName("methylationSubtype")]
     public MethylationSubtype? MethylationSubtype { get; set; }
+    [JsonPropertyName("gcimpMethylation")]
     public bool? GcimpMethylation { get; set; }
 }
