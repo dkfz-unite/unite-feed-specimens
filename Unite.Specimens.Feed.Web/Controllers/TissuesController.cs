53bb78ae935568df052bdcc20ba8dc7e8ceaddf4
DGitRich <daniel.rich@gmx.net>
2023-12-06 12:03:03 +0100

    Create TissuesController

---
+++ Unite.Specimens.Feed.Web/Controllers/TissuesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Unite.Specimens.Feed.Data.Specimens;
+using Unite.Specimens.Feed.Data.Specimens.Exceptions;
+using Unite.Specimens.Feed.Web.Configuration.Constants;
+using Unite.Specimens.Feed.Web.Models;
+using Unite.Specimens.Feed.Web.Models.Base.Converters;
+using Unite.Specimens.Feed.Web.Models.Binders;
+using Unite.Specimens.Feed.Web.Services;
+
+namespace Unite.Specimens.Feed.Web.Controllers;
+
+[Route("api/[controller]")]
+[Authorize(Policy = Policies.Data.Writer)]
+public class TissuesController : Controller
+{
+    private readonly SpecimenDataWriter _dataWriter;
+    private readonly SpecimenIndexingTasksService _indexingTaskService;
+    private readonly ILogger _logger;
+
+    private readonly SpecimenDataModelConverter _converter;
+
+
+    public TissuesController(
+        SpecimenDataWriter dataWriter,
+        SpecimenIndexingTasksService indexingTaskService,
+        ILogger<SpecimensController> logger)
+    {
+        _dataWriter = dataWriter;
+        _indexingTaskService = indexingTaskService;
+        _logger = logger;
+
+        _converter = new SpecimenDataModelConverter();
+    }
+
+    [HttpPost("tsv")]
+    [Consumes("text/tab-separated-values")]
+    public IActionResult PostTsv([ModelBinder(typeof(TissuesTsvModelBinder))] SpecimenDataModel[] models)
+    {
+        try
+        {
+            var dataModels = models.Select(model => _converter.Convert(model));
+
+            _dataWriter.SaveData(dataModels, out var audit);
+
+            _logger.LogInformation(audit.ToString());
+
+            _indexingTaskService.PopulateTasks(audit.Specimens);
+
+            return Ok();
+        }
+        catch (NotFoundException exception)
+        {
+            _logger.LogError(exception.Message);
+
+            return BadRequest(exception.Message);
+        }
+    }
+}
