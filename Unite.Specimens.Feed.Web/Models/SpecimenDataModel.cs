b7dd65a52827d7e4593926984b5fe346fd45b01e
DGitRich <daniel.rich@gmx.net>
2023-12-06 12:05:10 +0100

    Update SpecimenDataModel

--- Unite.Specimens.Feed.Web/Models/SpecimenDataModel.cs
+++ Unite.Specimens.Feed.Web/Models/SpecimenDataModel.cs
@@ -1,4 +1,5 @@
-﻿using Unite.Specimens.Feed.Web.Models.Base.Enums;
+﻿using System.Text.Json.Serialization;
+using Unite.Specimens.Feed.Web.Models.Base.Enums;
 
 namespace Unite.Specimens.Feed.Web.Models;
 
@@ -12,18 +13,26 @@ public record SpecimenDataModel
     private int? _creationDay;
 
 
+    [JsonPropertyName("id")]
     public string Id { get => _id?.Trim(); init => _id = value; }
+    [JsonPropertyName("parentId")]
     public string ParentId { get => _parentId?.Trim(); init => _parentId = value; }
+    [JsonPropertyName("parentType")]
     public SpecimenType? ParentType { get => _parentType; init => _parentType = value; }
+    [JsonPropertyName("donorId")]
     public string DonorId { get => _donorId?.Trim(); init => _donorId = value; }
+    [JsonPropertyName("creationDate")]
     public DateTime? CreationDate { get => _creationDate; init => _creationDate = value; }
+    [JsonPropertyName("creationDay")]
     public int? CreationDay { get => _creationDay; init => _creationDay = value; }
 
+    [JsonPropertyName("tissue")]
     public Base.TissueModel Tissue { get; init; }
     public Base.CellLineModel CellLine { get; init; }
     public Base.OrganoidModel Organoid { get; init; }
     public Base.XenograftModel Xenograft { get; init; }
 
+    [JsonPropertyName("molecularData")]
     public Base.MolecularDataModel MolecularData { get; init; }
     public Base.DrugScreeningModel[] DrugsScreeningData { get; init; }
 }
