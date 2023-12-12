e94b42b9900b0d10480b6d72e50b98b06b8ac004
DGitRich <daniel.rich@gmx.net>
2023-12-12 19:05:09 +0100

    Update TissuesTsvModelBinder

--- Unite.Specimens.Feed.Web/Models/Binders/TissuesTsvModelBinder.cs
+++ Unite.Specimens.Feed.Web/Models/Binders/TissuesTsvModelBinder.cs
@@ -16,20 +16,20 @@ public class TissuesTsvModelBinder : IModelBinder
 
         var map = new ClassMap<SpecimenDataModel>()
             .Map(entity => entity.Id, "id")
-            .Map(entity => entity.DonorId, "donorId")
-            .Map(entity => entity.ParentId, "parentId")
-            .Map(entity => entity.ParentType, "parentType")
-            .Map(entity => entity.CreationDate, "creationDate")
-            .Map(entity => entity.CreationDay, "creationDay")
+            .Map(entity => entity.DonorId, "donor_id")
+            .Map(entity => entity.ParentId, "parent_id")
+            .Map(entity => entity.ParentType, "parent_type")
+            .Map(entity => entity.CreationDate, "creation_date")
+            .Map(entity => entity.CreationDay, "creation_day")
             .Map(entity => entity.Tissue.Type, "type")
-            .Map(entity => entity.Tissue.TumorType, "tumorType")
+            .Map(entity => entity.Tissue.TumorType, "tumor_type")
             .Map(entity => entity.Tissue.Source, "source")
-            .Map(entity => entity.MolecularData.MgmtStatus, "mgmtStatus")
-            .Map(entity => entity.MolecularData.IdhStatus, "idhStatus")
-            .Map(entity => entity.MolecularData.IdhMutation, "idhMutation")
-            .Map(entity => entity.MolecularData.GeneExpressionSubtype, "geneExpressionSubtype")
-            .Map(entity => entity.MolecularData.MethylationSubtype, "methylationSubtype")
-            .Map(entity => entity.MolecularData.GcimpMethylation, "gcimpMethylation");
+            .Map(entity => entity.MolecularData.MgmtStatus, "mgmt_status")
+            .Map(entity => entity.MolecularData.IdhStatus, "idh_status")
+            .Map(entity => entity.MolecularData.IdhMutation, "idh_mutation")
+            .Map(entity => entity.MolecularData.GeneExpressionSubtype, "gene_expression_subtype")
+            .Map(entity => entity.MolecularData.MethylationSubtype, "methylation_subtype")
+            .Map(entity => entity.MolecularData.GcimpMethylation, "gcimp_methylation");
 
         var model = TsvReader.Read(tsv, map).ToArray();
 
