4555163390e257f08b26702f6c756fe79bf0b810
DGitRich <daniel.rich@gmx.net>
2024-01-15 15:44:39 +0100

    Create DrugScreeningDataFlatModel

---
+++ Unite.Specimens.Feed.Web/Models/DrugScreeningDataFlatModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+using Unite.Specimens.Feed.Web.Models.Base;
+using Unite.Specimens.Feed.Web.Models.Base.Enums;
+
+namespace Unite.Specimens.Feed.Web.Models;
+
+public record DrugScreeningDataFlatModel : DrugScreeningModel
+{
+    private string _donorId;
+    private string _specimenId;
+    private SpecimenType _specimenType;
+
+
+    [JsonPropertyName("donor_id")]
+    public string DonorId { get => _donorId?.Trim(); init => _donorId = value; }
+
+    [JsonPropertyName("specimen_id")]
+    public string SpecimenId { get => _specimenId?.Trim(); init => _specimenId = value; }
+
+    [JsonPropertyName("specimen_type")]
+    public SpecimenType SpecimenType { get => _specimenType; init => _specimenType = value; }
+}
