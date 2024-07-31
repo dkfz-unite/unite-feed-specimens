4ef4b3d100aab4ad3fc5fc8d59cbd8895cfa6b03
vulasau <valiantsin.ulasau@gmail.com>
2024-07-31 10:03:53 +0200

    Fixed sample finding issue.

--- Unite.Specimens.Feed/Data/Repositories/SampleRepository.cs
+++ Unite.Specimens.Feed/Data/Repositories/SampleRepository.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Unite.Data.Context;
 using Unite.Data.Entities.Specimens.Analysis;
+using Unite.Data.Entities.Specimens.Enums;
 using Unite.Specimens.Feed.Data.Models;
 
 namespace Unite.Specimens.Feed.Data.Repositories;
@@ -28,9 +29,12 @@ internal class SampleRepository
 
     public Sample Find(SampleModel model)
     {
+        var specimenType = GetSpecimenType(model.Specimen);
+
         return _dbContext.Set<Sample>().AsNoTracking().FirstOrDefault(entity =>
-            entity.Specimen.ReferenceId == model.Specimen.Donor.ReferenceId &&
+            entity.Specimen.Donor.ReferenceId == model.Specimen.Donor.ReferenceId &&
             entity.Specimen.ReferenceId == model.Specimen.ReferenceId &&
+            entity.Specimen.TypeId == specimenType &&
             entity.Analysis.TypeId == model.Analysis.Type
         );
     }
@@ -59,5 +63,19 @@ internal class SampleRepository
             Day = model.Day,
             Parameters = model.Parameters
         };
+    }
+
+    private static SpecimenType GetSpecimenType(SpecimenModel model)
+    {
+        if (model is MaterialModel)
+            return SpecimenType.Material;
+        else if (model is LineModel)
+            return SpecimenType.Line;
+        else if (model is OrganoidModel)
+            return SpecimenType.Organoid;
+        else if (model is XenograftModel)
+            return SpecimenType.Xenograft;
+        else
+            throw new ArgumentException("Invalid specimen type");
     }
 }
