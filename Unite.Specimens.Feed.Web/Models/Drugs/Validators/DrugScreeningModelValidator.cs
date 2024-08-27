6ce457dbab354af21301babfe8292a9265597f1a
vulasau <valiantsin.ulasau@gmail.com>
2024-08-27 15:04:19 +0200

    Updated drug screening model validator.

--- Unite.Specimens.Feed.Web/Models/Drugs/Validators/DrugScreeningModelValidator.cs
+++ Unite.Specimens.Feed.Web/Models/Drugs/Validators/DrugScreeningModelValidator.cs
@@ -14,44 +14,105 @@ public class DrugScreeningModelValidator : AbstractValidator<DrugScreeningModel>
             .MaximumLength(255)
             .WithMessage("Maximum length is 255");
 
+
         RuleFor(model => model.Gof)
-            .Must(value => value >= 0 && value <= 1)
+            .GreaterThanOrEqualTo(0)
+            .LessThanOrEqualTo(1)
+            .When(model => model.Gof != null)
             .WithMessage("Should be in range [0, 1]");
+
 
         RuleFor(model => model.Dss)
-            .Must(value => value >= 0 && value <= 1)
+            .GreaterThanOrEqualTo(0)
+            .LessThanOrEqualTo(1)
+            .When(model => model.Dss != null)
             .WithMessage("Should be in range [0, 1]");
 
+
         RuleFor(model => model.DssS)
-            .Must(value => value >= -1 && value <= 1)
+            .GreaterThanOrEqualTo(-1)
+            .LessThanOrEqualTo(1)
+            .When(model => model.DssS != null)
             .WithMessage("Should be in range [-1, 1]");
 
+
         RuleFor(model => model.MinDose)
-            .Must(value => value > 0)
+            .GreaterThanOrEqualTo(0)
+            .When(model => model.MinDose != null)
+            .WithMessage("Should be greater than 0");
+
+        RuleFor(model => model.MinDose)
+            .LessThan(model => model.MaxDose)
+            .When(model => model.MinDose != null && model.MaxDose != null)
+            .WithMessage("Should be less than 'MaxDose'");
+
+
+        RuleFor(model => model.MaxDose)
+            .GreaterThanOrEqualTo(0)
+            .When(model => model.MaxDose != null)
             .WithMessage("Should be greater than 0");
 
         RuleFor(model => model.MaxDose)
-            .Must(value => value > 0)
-            .WithMessage("Should be greater than 0");
+            .GreaterThan(model => model.MinDose)
+            .When(model => model.MaxDose != null && model.MinDose != null)
+            .WithMessage("Should be greater than 'MinDose'");
+
 
         RuleFor(model => model.Dose25)
-            .Must(value => value > 0)
-            .WithMessage("Should be greater than 0");
+            .GreaterThanOrEqualTo(0)
+            .When(model => model.Dose25 != null)
+            .WithMessage("Should be greater than or euqal to 0");
+
+        RuleFor(model => model.Dose25)
+            .LessThanOrEqualTo(model => model.Dose50)
+            .When(model => model.Dose25 != null && model.Dose50 != null)
+            .WithMessage("Should be less than or equal to 'Dose50'");
+
+        RuleFor(model => model.Dose25)
+            .GreaterThanOrEqualTo(model => model.MinDose)
+            .LessThanOrEqualTo(model => model.MaxDose)
+            .When(model => model.Dose25 != null && model.MinDose != null && model.MaxDose != null)
+            .WithMessage("Should be in range ['MinDose', 'MaxDose']");
+
 
         RuleFor(model => model.Dose50)
-            .Must(value => value > 0)
-            .WithMessage("Should be greater than 0");
+            .GreaterThanOrEqualTo(0)
+            .When(model => model.Dose50 != null)
+            .WithMessage("Should be greater than or euqal to 0");
+
+        RuleFor(model => model.Dose50)
+            .LessThanOrEqualTo(model => model.Dose75)
+            .When(model => model.Dose50 != null && model.Dose75 != null)
+            .WithMessage("Should be less than or equal to Dose75");
+
+        RuleFor(model => model.Dose50)
+            .GreaterThanOrEqualTo(model => model.MinDose)
+            .LessThanOrEqualTo(model => model.MaxDose)
+            .When(model => model.Dose50 != null && model.MinDose != null && model.MaxDose != null)
+            .WithMessage("Should be in range ['MinDose', 'MaxDose']");
+
 
         RuleFor(model => model.Dose75)
-            .Must(value => value > 0)
-            .WithMessage("Should be greater than 0");
+            .GreaterThanOrEqualTo(0)
+            .When(model => model.Dose75 != null)
+            .WithMessage("Should be greater than or euqal to 0");
+        
+        RuleFor(model => model.Dose75)
+            .GreaterThanOrEqualTo(model => model.MinDose)
+            .LessThanOrEqualTo(model => model.MaxDose)
+            .When(model => model.Dose75 != null && model.MinDose != null && model.MaxDose != null)
+            .WithMessage("Should be in range ['MinDose', 'MaxDose']");
+
 
         RuleForEach(model => model.Doses)
-            .Must(value => value > 0)
+            .GreaterThan(0)
+            .When(model => model.Doses != null)
             .WithMessage("Should be greater than 0");
 
+
         RuleForEach(model => model.Responses)
-            .Must(value => value >= -1 && value <= 1)
-            .WithMessage("Should be in range [-1, 1]");
+            .GreaterThanOrEqualTo(0)
+            .When(model => model.Responses != null)
+            .WithMessage("Should be greater than or euqal to 0");
     }
 }
