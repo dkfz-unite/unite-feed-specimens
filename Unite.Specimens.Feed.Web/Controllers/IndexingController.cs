3974c2c13ea480c74e9b6e7e91331c0a3fb4ed57
vulasau <valiantsin.ulasau@gmail.com>
2025-03-27 16:02:00 +0100

    Updated reindexing rules.

--- Unite.Specimens.Feed.Web/Controllers/IndexingController.cs
+++ Unite.Specimens.Feed.Web/Controllers/IndexingController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Unite.Indices.Context;
+using Unite.Indices.Entities.Specimens;
 using Unite.Specimens.Feed.Web.Configuration.Constants;
 using Unite.Specimens.Feed.Web.Services;
 
@@ -9,17 +11,21 @@ namespace Unite.Specimens.Feed.Web.Controllers;
 [Authorize(Policy = Policies.Data.Writer)]
 public class IndexingController : Controller
 {
+    private readonly IIndexService<SpecimenIndex> _indexService;
     private readonly SpecimenIndexingTasksService _tasksService;
 
     public IndexingController(
+        IIndexService<SpecimenIndex> indexService,
         SpecimenIndexingTasksService tasksService)
     {
+        _indexService = indexService;
         _tasksService = tasksService;
     }
 
     [HttpPost]
     public IActionResult Post()
     {
+        _indexService.DeleteIndex().Wait();
         _tasksService.CreateTasks();
         
         return Ok();
